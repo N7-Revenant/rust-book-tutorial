02978bfc3e7b561be53701bd36bfa2e8f36d1917
Dmitrii Zolnikov <dmitri.zol@inbox.ru>
2023-11-02 17:37:41 +0300

    Добавлены исходный код 5-го упражнения 3-й главы

---
+++ projects/ch03/branches/src/main.rs
@@ -0,0 +1,81 @@
+fn check (number: i32) {
+    let mut condition = true;
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+    if number != 0 {
+        println!("number was something other than zero")
+    }
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3 or 2");
+        condition = false
+    }
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+}
+
+fn main() {
+    check(3);
+    check(7);
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("The value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
